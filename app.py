b98b9b2501ef48b5ba15c5974c2da376163b37ab
arjun-ms <arjunachu123.aa@gmail.com>
2023-05-31 22:27:08 +0530

    feat: Add Image Prediction in Streamlit

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 import streamlit as st
+from fastai.vision.all import *
+from PIL import Image
+
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
 
 # Set page title
 st.set_page_config(page_title="Wing Watch")
+
+# Image Prediction
+def predict(path):
+    model_inf = load_learner('525-species-v1.pkl')
+    pred_class = model_inf.predict(path)
+    # print(pred_class[0])
+    return pred_class[0]
+
 
 # Define page content
 def page_content():
@@ -19,6 +33,16 @@ def page_content():
             # Check if at least one file is uploaded before displaying success message
             if uploaded_image_file is not None or uploaded_audio_file is not None:
                 st.success("Files uploaded")
+                st.write(uploaded_image_file)
+                print(type(uploaded_image_file))
+                if uploaded_image_file is not None:
+                    img = Image.open(uploaded_image_file)
+                    with open(f"test/{uploaded_image_file.name}","wb") as f:
+                        f.write(uploaded_image_file.getbuffer())
+                    # Perform prediction
+                    prediction = predict(f"test/{uploaded_image_file.name}")
+                    # Display the predicted class
+                    st.write("Predicted class: ", prediction)
             else:
                 st.error("Please upload an image or audio file")
     else:
