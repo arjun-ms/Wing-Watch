b472a1871fef39c1afb06c828bdf0293c80b39fe
arjun-ms <arjunachu123.aa@gmail.com>
2023-06-01 18:44:57 +0530

    feat: More Details about the bird

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import pathlib
 temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
 
+from details import find_details
+
 # Set page title
 st.set_page_config(page_title="Wing Watch")
 
@@ -20,7 +22,7 @@ def predict(path):
 # Define page content
 def page_content():
     # Display page header
-    st.header("Wing Watch")
+    st.title("Wing Watch 🦜")
 
     # Display file uploader widgets for image and audio files
     uploaded_image_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
@@ -33,16 +35,18 @@ def page_content():
             # Check if at least one file is uploaded before displaying success message
             if uploaded_image_file is not None or uploaded_audio_file is not None:
                 st.success("Files uploaded")
-                st.write(uploaded_image_file)
-                print(type(uploaded_image_file))
+                # st.write(uploaded_image_file)
+                # print(type(uploaded_image_file))
                 if uploaded_image_file is not None:
                     img = Image.open(uploaded_image_file)
                     with open(f"test/{uploaded_image_file.name}","wb") as f:
                         f.write(uploaded_image_file.getbuffer())
                     # Perform prediction
                     prediction = predict(f"test/{uploaded_image_file.name}")
+                    details = find_details(prediction)
                     # Display the predicted class
-                    st.write("Predicted class: ", prediction)
+                    st.markdown(f"The predicted bird is: **{prediction}**", )
+                    st.markdown(details)
             else:
                 st.error("Please upload an image or audio file")
     else:
